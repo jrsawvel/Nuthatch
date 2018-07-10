143a1085af06d78892d7359216c350675a96946f
root <root@soupmode.com>
2018-07-10 21:53:10 +0000

    completed search code v1

---
+++ elasticsearch/dslsearch.sh
@@ -0,0 +1,9 @@
+curl -X GET "http://127.0.0.1:9200/veerydvlp1/veerydvlp1/_search" -H 'Content-Type: application/json' -d'
+{
+    "query": {
+        "match_phrase": {
+            "title": "My Web Apps as of August 2015"
+        }
+    }
+}
+'
--- lib/API/searchposts.lua
+++ lib/API/searchposts.lua
@@ -5,6 +5,7 @@ local M = {}
 local cgilua  = require "cgilua"
 local urlcode = require "cgilua.urlcode"
 local cjson   = require "cjson"
+local rex     = require "rex_pcre"
 
 local config  = require "config"
 local utils   = require "utils"
@@ -56,7 +57,7 @@ function M.do_tag_search(a_params)
             local next_link_bool = 0
 
             if #stream > max_entries then
-                    next_link_bool = 1
+                next_link_bool = 1
             end
 
             local posts = {}
@@ -87,11 +88,12 @@ end
 
 function M.do_string_search(a_params)
 
+    local page_num = 1
+
     if a_params[3] == nil then
         rj.report_error("400", "Missing data.", "Enter keyword(s) to search.")
     else
         local keyword = a_params[3]
-        local page_num = 1
 
         if cgilua.QUERY.page ~= nil and utils.is_numeric(cgilua.QUERY.page) == true then
             page_num = cgilua.QUERY.page
@@ -104,70 +106,76 @@ function M.do_string_search(a_params)
         else
             keyword = urlcode.unescape(keyword)
             keyword = keyword:gsub("+", " ")
-            rj.report_error("400", "DEBUG", keyword)
-        end
-    end
-end
+            -- remove unnacceptable chars from the search string
+            -- keyword = rex.gsub(keyword, "[^A-Za-z0-9 _'%-%#%.]", "", nil, "sx")
 
+            local db = config.get_value_for("database_name")
 
-function M.xxxsearches(a_params)
+            local max_entries = config.get_value_for("max_entries_on_page")
 
-    local author_name = cgilua.QUERY.author
-    local session_id  = cgilua.QUERY.session_id
-    local rev         = cgilua.QUERY.rev
+            local skip_count = (max_entries * page_num) - max_entries
 
-    if auth.is_valid_login(author_name, session_id, rev) == false then 
-        rj.report_error("400", "Unable to peform action.", "You are not logged in.")
-    else
-        local posts = {}
+-- local url = 'http://127.0.0.1:5984/' .. db .. '/_design/views/_view/tag_search?descending=true&limit=' .. max_entries + 1 .. '&skip=' .. math.tointeger(skip_count)
 
-        local total_hits = 0
-  
-        local search_text = a_params[2]
+            local url = 'http://127.0.0.1:9200/' .. db .. '/' .. db .. '/_search?size=' .. max_entries + 1 .. '&q=%2Btype%3Apost+%2Bpost_status%3Apublic+%2Bmarkup%3A' .. urlcode.escape(keyword) .. '&from=' .. math.tointeger(skip_count)
 
-        search_text = urlcode.unescape(search_text)
+            local response = utils.get_unsecure_web_page(url)
 
-        -- remove unnacceptable chars from the search string
-        search_text = rex.gsub(search_text, "[^A-Za-z0-9 _'%-%#%.]", "", nil, "sx")
+            -- convert the json text into a lua table (hashes and arrays)
+            local t = cjson.decode(response)
 
-        local default_doc_root = config.get_value_for("default_doc_root")
+            local total_hits = math.tointeger(t.hits.total)
 
-        local search_results_filename = config.get_value_for("searches_storage") .. "/" .. os.time() .. ".txt"
-
-        local grep_cmd = "grep -i -R --exclude-dir=versions --include='*.txt' -m 1 '" .. search_text .. "' " .. default_doc_root .. " > " .. search_results_filename
-
-        local r = os.execute(grep_cmd)
-
-        if r == true then
-            local f = io.open(search_results_filename, "r")
-
-            if f == nil then
-                rj.report_error("400", "Could not open search results file for read.", "")
+            if total_hits == 0 then
+                -- no search results found. but it's not an error. it's up to the client to deal with this fact.
+                local hash = {}
+                hash.posts = nil
+                rj.success(hash)               
             else
-                local home_page = config.get_value_for("home_page")
-                for line in f:lines() do
-                    local tmp_array = utils.split(line, ".txt:")
-                    local tmp_str = rex.gsub(tmp_array[1], default_doc_root .. "/" , "")
-                    local tmp_hash = {
-                        uri = tmp_str,
-                        url = home_page .. "/" .. tmp_str .. ".html"
-                    }
-                    table.insert(posts, tmp_hash)
-                    total_hits = total_hits + 1
-                end
-                f:close()
-                local hash = {
-                    total_hits = total_hits,
-                    search_text = search_text,
-                    posts = posts
-                }
-                
-                rj.success(hash) 
-            end
-        else
-            rj.report_error("400", "Unable to execute search.", grep_cmd)
-        end
+                local stream = t.hits.hits
 
+                local next_link_bool = 0
+
+                if #stream > max_entries then
+                    next_link_bool = 1
+                end
+
+                local posts = {}
+
+                for i=1, #stream do
+                   -- stream[i]._source.formatted_updated_at = utils.format_date_time(stream[i]._source.updated_at)
+
+                    posts[i] = stream[i]._source
+
+                    posts[i].formatted_updated_at = utils.format_date_time(posts[i].updated_at)
+
+                    posts[i].slug = posts[i]._id
+
+                    if #posts[i].tags < 1 then
+                        posts[i].tags[1] = ""
+                    end
+
+                    posts[i]._id = nil
+                    posts[i].created_at = nil
+                    posts[i].html = nil
+                    posts[i].markup = nil
+                    posts[i]._rev = nil
+                    posts[i].post_status = nil
+                    posts[i].type = nil
+                    posts[i].word_count = nil
+
+                    if i == max_entries then
+                        break
+                    end
+                end
+
+                local hash = {}
+                hash.posts = posts
+                hash.next_link_bool = next_link_bool
+                rj.success(hash)
+            end
+
+        end
     end
 end
 
